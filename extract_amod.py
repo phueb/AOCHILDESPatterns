ec637844bd2eac206306fcd4cf781f445154c64d
phueb <info@philhuebner.com>
2021-11-03 16:21:29 -0500

    get amod with greatest percent increase

--- extract_amod.py
+++ extract_amod.py
@@ -1,14 +1,22 @@
+"""
+Find all spans in AO-CHILDES where a probe is preceded by a token tagged by spacy as "amod".
+
+Save results to a txt file for plotting in Latex
+
+"""
 import spacy
 from spacy.tokens import Doc
 from spacy.matcher import Matcher
 from typing import List, Generator
+from collections import defaultdict, Counter
 
 from aochildes.dataset import ChildesDataSet
 from aochildespatterns.utils import save_summary_to_txt
 from aochildespatterns.probes import probes
 
-NUM_PARTS = 64
+NUM_PARTS = 2
 PATTERN_NAME = 'amod+PROBE'
+VERBOSE = False
 
 transcripts_ = ChildesDataSet().load_transcripts()
 
@@ -38,7 +46,8 @@ def gen_spans_by_partition(texts: List[str]) -> Generator[List[str], None, None]
         matches = matcher(doc)
         for match_id, start, end in matches:
             span = doc[start:end]
-            print(span.text)
+            if VERBOSE:
+                print(span.text)
             spans.append(span.text)
 
         # yield spans only when a whole partition worth of transcripts have been processed, then clear spans
@@ -50,6 +59,8 @@ def gen_spans_by_partition(texts: List[str]) -> Generator[List[str], None, None]
 
 y1 = []
 y2 = []
+part_id2amods = defaultdict(list)
+amod2spans = defaultdict(list)
 for part_id, spans_in_part in enumerate(gen_spans_by_partition(transcripts)):
     y1i = len(spans_in_part)
     y2i = len(set(spans_in_part)) / len(spans_in_part)
@@ -58,16 +69,37 @@ for part_id, spans_in_part in enumerate(gen_spans_by_partition(transcripts)):
 
     print(f'Partition {part_id:>6,} | Found {y1i :>6,} {PATTERN_NAME} spans of which {y2i:>6,} are unique')
 
+    # collect all amods to see which become more frequent with age
+    for span in spans_in_part:
+        amod = span.split()[-2]
+        part_id2amods[part_id].append(amod)
+        amod2spans[amod].append((part_id, span))
 
-x1 = [i + 1 for i in range(len(y1))]
-x2 = [i + 1 for i in range(len(y2))]
 
-save_summary_to_txt(x=x1,
+# which amod has greatest percent increase from part 1 to part 2?
+if NUM_PARTS == 2:
+    c0 = Counter(part_id2amods[0])
+    c1 = Counter(part_id2amods[1])
+
+    amod2pi = {}
+    for amod in c1:
+        f0 = c0.get(amod, 1)  # pretend each amod in part 2 is seen at least once in part 1
+        f1 = c1[amod]
+        fd = f1 - f0
+        percent_increase = fd / f0
+        print(f'{amod:<16} f0={f0:>6,} f1={f1:>6,} fd={fd:>6,} pi={percent_increase:.3f}')
+        amod2pi[amod] = percent_increase
+
+    for amod, pi in sorted(amod2pi.items(), key=lambda i: i[1])[-10:]:
+        print(f'{amod:<16} pi={pi:.4f}')
+        print(amod2spans[amod])
+
+# summaries
+save_summary_to_txt(x=[i + 1 for i in range(len(y1))],
                     y=y1,
                     quantity_name=f'num_occurrences_of_{PATTERN_NAME}',
                     )
-
-save_summary_to_txt(x=x1,
+save_summary_to_txt(x=[i + 1 for i in range(len(y2))],
                     y=y2,
                     quantity_name=f'percent_unique_of_{PATTERN_NAME}',
                     )
